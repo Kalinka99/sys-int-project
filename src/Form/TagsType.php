537b31b9153fa15633cf388309330a52f39bf8ca
Kalinka99 <62285432+Kalinka99@users.noreply.github.com>
2021-06-25 01:31:09 +0200

    ulepszone formularze

--- src/Form/TagsType.php
+++ src/Form/TagsType.php
@@ -4,17 +4,22 @@ namespace App\Form;
 
 use App\Entity\Tags;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class TagsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('articles')
-        ;
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Uzupełnij to pole.'
+                    ])]])
+            ->add('articles');
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Form/UsersType.php
+++ src/Form/UsersType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,7 +16,9 @@ class UsersType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
+            ->add('email', EmailType::class, [
+                'invalid_message' => 'Uwzględnij znak @ w adresie e-mail. '
+            ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'required' => false,

da63b16d98c9564ca6d7ed26fc5b1eb16d0adb4e
Kalinka99 <62285432+Kalinka99@users.noreply.github.com>
2021-07-09 23:53:58 +0200

    poprawki

--- src/Form/TagsType.php
+++ src/Form/TagsType.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Kalina Muchowicz <https://github.com/Kalinka99>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
 namespace App\Form;
 
 use App\Entity\Tags;
